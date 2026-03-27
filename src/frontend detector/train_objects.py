e1753eb761191516be31b9e7624019e78b588443
Lucas Moreira <lucas.godoy@edu.fecap.br>
2026-03-26 21:47:13 -0300

    add dataset #1

--- src/frontend detector/train_objects.py
+++ src/frontend detector/train_objects.py
@@ -8,5 +8,5 @@ results = model.train(
     data='data.yaml', # Aponta para o arquivo .yaml
     epochs=50,            # Número de vezes que o modelo vai processar todo o dataset
     imgsz=640,            # Redimensiona as imagens para 640x640 pixels (padrão do YOLO)
-    name='modelo_alimentos' # Nome da pasta onde os resultados serão salvos
+    name='treino_alimentos' # Nome da pasta onde os resultados serão salvos
 )
